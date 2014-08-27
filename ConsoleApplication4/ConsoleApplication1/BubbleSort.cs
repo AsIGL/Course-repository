f7c19e9b1911eafbe3fbe97a355fcbdc54519bbc
AsIGL <ignas.lapienis@gmail.com>
2014-08-27 14:49:19 +0200

    Project for algos
    Algos

---
+++ ConsoleApplication4/ConsoleApplication1/BubbleSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace BubbleSort
+{
+    public class BubbleSort
+    {
+        public static void BubbleSortAlgo(int[] ListOfNumbers)
+        {
+            int LengthOfAnArray = ListOfNumbers.Length;
+
+            for (int n = 0; n < LengthOfAnArray; n++)
+            {
+                Console.WriteLine("New ");
+                for (int i = 1; i < (ListOfNumbers.Length); i++)
+                {
+                    if (ListOfNumbers[i - 1] > ListOfNumbers[i])
+                    {
+                        int temp = ListOfNumbers[i];
+                        ListOfNumbers[i] = ListOfNumbers[i - 1];
+                        ListOfNumbers[i - 1] = temp;
+                    }
+                    PrintArray.Printing(ListOfNumbers);
+                }
+            }
+
+        }
+
+
+
+        public class PrintArray
+        {
+            public static void Printing(int[] printArrayIs)
+            {
+
+                Console.Write(" Array is: ");
+
+                foreach (var item in printArrayIs)
+                {
+                    Console.Write(" " + item);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+
+
+}
---
+++ ConsoleApplication4/ConsoleApplication4/Algoritms.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritms
+{
+    class AlgorithmsTest
+    {
+        static int[] TestArray = {68,10,50,17 };
+        static int[] RandomNumberArray = new int[100] ;
+
+        static void Main(string[] args)
+        {
+           
+
+            //BubbleSort.BubbleSort.PrintArray.Printing(TestArray);
+            //BubbleSort.BubbleSort.BubbleSortAlgo(TestArray);
+
+            RandomNumberGenerator.RandomArraygenerator(ref RandomNumberArray);
+
+            BubbleSort.BubbleSort.BubbleSortAlgo(RandomNumberArray);
+            BubbleSort.BubbleSort.PrintArray.Printing(RandomNumberArray);                        
+            Console.WriteLine(" Algo done ");
+        }
+    }
+    
+    
+
+    public static class RandomNumberGenerator
+    {  
+        public static void RandomArraygenerator(ref int[] RandomArray)
+        {
+            Random rnd = new Random();           
+            for (int i = 0; i < RandomArray.Length; i++)
+            {
+                RandomArray[i] = rnd.Next(1, 101);               
+            }
+        }
+    }
+}
